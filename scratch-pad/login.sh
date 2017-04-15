6df6ff552eac44421d1ae6801786e229fbdb39a8
Wes Maldonado <wes@brokenbuild.com>
2017-04-15 11:03:15 -0700

    Explored login

--- scratch-pad/login.sh
+++ scratch-pad/login.sh
@@ -3,4 +3,7 @@
 # note: demo-account-type has some interesting values
 # Values: "default", "overspent", "no-savings-account", "autosave-enabled"
 
-curl -H 'Accept: application/json' -H 'Content-Type: application/json' -X POST -d '{"email": "level@example.com", "password": "incorrect_password", "args": {"uid": 1110590645, "token": "DA2214B6BE2802EF03FEB1533EAB6D7C", "api-token": "AppTokenForInterview", "json-strict-mode": false, "json-verbose-response": false}, "demo-account-type": "default"}' https://2016.api.levelmoney.com/api/v2/core/login
+# Assume if I use my credentials I'll get my demo UID and the same token back... maybe not?
+curl -H 'Accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -X POST -d '{"email": "interview@levelmoney.comEROR", "password": "password2", "args": {"uid": 1110590645, "token": "DA2214B6BE2802EF03FEB1533EAB6D7C", "api-token": "AppTokenForInterview", "json-strict-mode": false, "json-verbose-response": false}, "demo-account-type": "default"}' https://2016.api.levelmoney.com/api/v2/core/login
