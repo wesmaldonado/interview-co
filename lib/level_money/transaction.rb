55eb739160249d65d9e7123ed436ec1d42ec2c79
Wes Maldonado <wes@brokenbuild.com>
2017-04-19 17:27:27 -0700

    Checkpointing the model work before the reports

--- lib/level_money/transaction.rb
+++ lib/level_money/transaction.rb
@@ -1,11 +1,64 @@
 module LevelMoney
 end
-
+class LevelMoney::MonthlyReport
+  # Expects LevelMoney::Transcactions
+  def initialize(transactions)
+    @ts = transactions
+  end
+  def to_report_data
+    # { "2017-04": {"spent": "$100.00", "income": "$0.00"}}
+    {}
+  end
+end
 class LevelMoney::Transaction
   def initialize(params)
     @params = params
   end
+  def to_usd
+    signifier = (amount < 0) ? '-' : ''
+    sprintf(signifier + '$%.2f', (amount / 10000.0).to_f.abs)
+  end
   def pending?
     @params['is-pending'] == 'true'
+  end
+  def transaction_id
+    @params['transaction-id']
+  end
+  def categorization 
+    @params['categorization']
+  end
+  def transaction_date
+    @transaction_date ||= DateTime.parse(@params['transaction-time'])
+  end
+
+  def amount
+    @amount ||= @params['amount'].to_i # Note: Use the money gem if we need it.
+  end
+
+  def debit?
+    0 >= amount
+  end
+
+  def credit?
+    ! debit?
+  end
+end
+
+class LevelMoney::Transactions
+  # We expect the `transactions` from the parsed JSON 
+  def self.from_api_client(transactions)
+    @transactions = transactions.map{ |t| LevelMoney::Transaction.new(t) }
+  end
+  def initialize(list = [])
+    @transactions= list
+  end
+  def <<(i)
+    @transactions<< i
+  end
+  def size
+    @transactions.size
+  end
+  def last
+    @transactions.last
   end
 end
--- spec/level_money_spec.rb
+++ spec/level_money_spec.rb
@@ -1,5 +1,36 @@
 require 'level_money/transaction.rb'
 require 'date'
+RSpec.describe LevelMoney::MonthlyReport do
+
+  Given(:transaction_data_from_api){
+      {"amount"=>-1000000, "is-pending"=>false, "payee-name-only-for-testing"=>"ATM WITHDRAWAL", "aggregation-time"=>1492449140661, "account-id"=>"nonce:comfy-checking/hdhehe", "clear-date"=>1492420260000, "memo-only-for-testing"=>"Example Memo", "transaction-id"=>"1492420260000", "raw-merchant"=>"ATM WITHDRAWAL", "categorization"=>"Cash & ATM", "merchant"=>"ATM Withdrawal", "transaction-time"=>"2017-04-17T00:00:00.000Z"}
+  }
+  it "produces an empty report" do
+    report = LevelMoney::MonthlyReport.new(LevelMoney::Transactions.from_api_client([]))
+    expect(report.to_report_data).to eq({})
+  end
+  it "produces the report" do
+    report = LevelMoney::MonthlyReport.new(LevelMoney::Transactions.from_api_client([transaction_data_from_api]))
+    pending("Next step after a commit.")
+    expect(report.to_report_data).to eq({ "2017-04": {"spent": "$100.00", "income": "$0.00"}})
+  end
+end
+RSpec.describe LevelMoney::Transactions do
+  Given(:transaction_data_from_api){
+      {"amount"=>-1000000, "is-pending"=>false, "payee-name-only-for-testing"=>"ATM WITHDRAWAL", "aggregation-time"=>1492449140661, "account-id"=>"nonce:comfy-checking/hdhehe", "clear-date"=>1492420260000, "memo-only-for-testing"=>"Example Memo", "transaction-id"=>"1492420260000", "raw-merchant"=>"ATM WITHDRAWAL", "categorization"=>"Cash & ATM", "merchant"=>"ATM Withdrawal", "transaction-time"=>"2017-04-17T00:00:00.000Z"}
+  } 
+
+  it "can load via json from the api" do
+    ts = LevelMoney::Transactions.from_api_client([transaction_data_from_api])
+    expect(ts.size).to eq(1)
+    expect(ts.first.transaction_id).to eq(LevelMoney::Transaction.new(transaction_data_from_api).transaction_id)
+  end
+  it "can load many via json from the api" do
+    ts = LevelMoney::Transactions.from_api_client([transaction_data_from_api, transaction_data_from_api, transaction_data_from_api])
+    expect(ts.size).to eq(3)
+    expect(ts.last.transaction_id).to eq(LevelMoney::Transaction.new(transaction_data_from_api).transaction_id)
+  end
+end
 
 RSpec.describe LevelMoney::Transaction do
   Given(:transaction_data_from_api){
@@ -20,5 +51,17 @@ RSpec.describe LevelMoney::Transaction do
   end
   it "has a categorization" do
     expect(transaction.categorization).to eq(transaction_data_from_api["categorization"])
+  end
+  it "has an amount in centocents" do
+    expect(transaction.amount).to eq(-1000000)
+  end
+  it "knows if it is a debit" do
+    expect(transaction).to be_debit
+  end
+  it "knows if it is a credit" do
+    expect(transaction).not_to be_credit
+  end
+  it "can report itself as a dollar amount" do
+    expect(transaction.to_usd).to eq("-$100.00")
   end
 end
