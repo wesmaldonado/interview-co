a5ed2bdc3b4ff8cba2881e4b3fbb4dd5fda94710
Wes Maldonado <wes@brokenbuild.com>
2017-04-20 17:58:22 -0700

    Review this code to find my notes.

--- scratch-pad/login.sh
+++
@@ -1,9 +0,0 @@
-# https://doc.level-labs.com/doc/index.html#login 
-
-# note: demo-account-type has some interesting values
-# Values: "default", "overspent", "no-savings-account", "autosave-enabled"
-
-# Assume if I use my credentials I'll get my demo UID and the same token back... maybe not?
-curl -H 'Accept: application/json' \
-        -H 'Content-Type: application/json' \
-        -X POST -d '{"email": "interview@levelmoney.comEROR", "password": "password2", "args": {"uid": 1110590645, "token": "DA2214B6BE2802EF03FEB1533EAB6D7C", "api-token": "AppTokenForInterview", "json-strict-mode": false, "json-verbose-response": false}, "demo-account-type": "default"}' https://2016.api.levelmoney.com/api/v2/core/login
